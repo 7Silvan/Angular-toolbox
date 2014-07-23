3eddb0fe20a83264ff7c7884661edca7cbb0181a
Roman Gural <roman.gural@pragmatists.pl>
2014-07-23 20:18:40 +0200

    resolved with manifest possibilities

--- js/StacktraceResolver.js
+++ js/StacktraceResolver.js
@@ -1,12 +1,8 @@
-//Chrome passes the error object (5th param) which we must use since it now truncates the Msg (1st param).
-window.onerror = function (errorMsg, url, lineNumber, columnNumber, errorObject) {
-    var errMsg;
-    //check the errorObject as IE and FF don't pass it through (yet)
-    if (errorObject && errorObject !== undefined) {
-        errMsg = errorObject.message;
-    }
-    else {
-        errMsg = errorMsg;
-    }
-    alert('Error: ' + errMsg);
-}
+(function() {
+    var s = document.createElement('script');
+    s.src = chrome.extension.getURL('js/ErrorHandler.js');
+    s.onload = function () {
+        this.parentNode.removeChild(this);
+    };
+    (document.head || document.documentElement).appendChild(s);
+})();
